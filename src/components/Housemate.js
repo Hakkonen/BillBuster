b6da923ff772cd42ea6df5483dbf63c57fd722b6
hakkonen <rnlds.j@gmail.com>
2021-03-31 16:07:18 +1100

    Updated colours
    Updated website colour scheme and added red highlight to housemate days if they exceed the bill period.

--- src/components/Housemate.js
+++ src/components/Housemate.js
@@ -1,6 +1,18 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+// import BillPeriod from "./BillPeriod"
 
 function Housemate(props) {
+    const [ stayLimit, setStayLimit ] = useState(false)
+
+    // Checks if person's stay has been inputted as higher than total bill length. Red effect is applied in JSX.
+    useEffect(() => {
+        if(props.person.stay > props.billPeriod) {
+            setStayLimit(true)
+        } else {
+            setStayLimit(false)
+        }
+    }, [props.person.stay])
+
     return (
         <div className="housemate">
             <div className="left-col">
@@ -20,12 +32,14 @@ function Housemate(props) {
                     style={{height: "7.5%"}}
                 >
                     <input
+                        id={"Stay_Input_" + props.person.key}
                         type="text"
                         name="period"
                         value={props.person.stay}
                         onChange={e => props.handleChange(e, props.person.key)}
                         maxLength="3"
                         placeholder="30"
+                        style={{color: stayLimit ? "red" : "#000"}}
                     ></input>
                     <p className="days">DAYS</p>
                 </div>
@@ -36,34 +50,34 @@ function Housemate(props) {
                     className="arrow"
                     style={{height: "30%"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
                 
                 <svg 
                     className="arrow-connector"
                     style={{height: "2px"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="300" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
-                    <line x1={-props.width/3} y1="0" x2="20" y2="0" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="300" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
+                    <line x1={-props.width/3} y1="0" x2="20" y2="0" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
 
                 <svg  
                     className="arrow"
                     style={{height: "20%"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
 
                 <svg 
                     className="arrow-connector"
                     style={{height: "2px"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="300" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
-                    <line x1={-props.width/3} y1="0" x2="20" y2="0" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="300" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
+                    <line x1={-props.width/3} y1="0" x2="20" y2="0" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg> 
 
                 <svg  className="arrow">
-                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
             </div>
         </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -25,26 +25,26 @@ function Results(props) {
                     className="arrow"
                     style={{height: "40%"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
 
                 <svg 
                     className="arrow-connector"
                     style={{height: "2px"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="300" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
-                    <line x1={-props.width/3} y1="0" x2="20" y2="0" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="300" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
+                    <line x1={-props.width/3} y1="0" x2="20" y2="0" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
 
                 <svg  
                     className="arrow"
                     style={{height: "7.5%"}}
                 >
-                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
 
                 <svg  className="arrow">
-                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(255,255,255)", strokeWidth: "1"}} />
+                    <line x1="20" y1="0" x2="20" y2="900" style={{stroke: "rgb(0,0,0)", strokeWidth: "1"}} />
                 </svg>
             </div>
         </div>
